f8d2fc050f5327b2ccacce4df591609dbf3b29a0
Jose Rodriguez <jrodriguezballester@gmail.com>
2020-10-13 12:29:06 +0200

    Prog1

---
+++ prog1.py
@@ -0,0 +1,8 @@
+# Prog1: Escriba un programa que pida dos números y que escriba su media aritmética.
+
+# El método int () devuelve un objeto entero de cualquier número o cadena.
+num1 = int(input("introduce un numero"))
+num2 = int(input('introduce un segundo numero'))
+
+media = (num1 + num2) / 2
+print(f"la media es {media}")
